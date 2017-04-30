bd305f25c1c58507e1e38dde5a1561135cc53d9e
dgattoni <danigattoni@gmail.com>
2017-04-30 10:33:16 -0300

    json files added

--- src/server/routes/search.js
+++ src/server/routes/search.js
@@ -1,12 +1,31 @@
 'use strict';
 
 const meta = require('../content/meta.js');
+const maipo = require('../data/maipo.json');
+const yarra = require('../data/yarra.json');
+let data;
 
 module.exports = function(app) {
   app.get('/search/:river', (req, res) => {
+    switch (req.params.river) {
+      case 'maipo':
+        data = maipo;
+      break;
+      case 'yarra':
+        data = yarra;
+      break;
+      case 'mapocho':
+        data = mapocho;
+      break;
+      default:
+        data = maipo;
+      break;
+    }
+
     res.render('home/river', {
       page: 'river',
-      meta: meta('home')
+      meta: meta('home'),
+      data: data[0] || []
     });
   });
 };
